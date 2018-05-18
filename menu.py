792d9836002dcc2ff0449709368e6a7a32e3ef45
kishor kumar <31623726+kishorkiku@users.noreply.github.com>
2018-05-19 00:45:21 +0530

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+option='''
+
+press 1: ACCESS SAAS :-->
+press 2: ACCESS STAAS :-->
+press 3: ACCESS IAAS :-->
+press 4: ACCESS PAAS :-->
+'''
+print option
+ch=int(raw_input())
+if ch==3:
+ print"IAAS service is loading ..."
+else:
+ print"not valid naumber"
